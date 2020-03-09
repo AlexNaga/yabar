9a8b7058bd1beca982e12bef085b61e5c4e91262
David Karlsson <davidkarlssson@gmail.com>
2020-03-09 11:48:11 +0100

    More distinguished color differentiation between focused and visible space in the workspace component

--- lib/styles/styles.jsx
+++ lib/styles/styles.jsx
@@ -1,16 +1,16 @@
 export default {
   colors: {
-    fg: 'rgba(255,255,255,0.75)',
-    dim: 'rgba(255,255,255,0.7)',
-    bg: '#2E3440',
-    red: 'rgba(221,51,0,1)',
-    accent: '#5fafaf',
-    lightgray: '#D8DEE9'
+    fg: "rgba(255,255,255,0.75)",
+    dim: "rgba(255,255,255,0.4)",
+    bg: "#2E3440",
+    red: "rgba(221,51,0,1)",
+    accent: "#5fafaf",
+    lightgray: "#D8DEE9"
   },
-  fontSize: '12px',
-  lineHeight: '24px',
+  fontSize: "12px",
+  lineHeight: "24px",
   fontWeight: 500,
   fontFamily: "'SF Mono', monospace",
-  iconSize: '0.8',
-  iconSpacing: '8px'
+  iconSize: "0.8",
+  iconSpacing: "8px"
 };
