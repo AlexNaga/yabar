70293b2d367a34b54256c9454a5153d7fb5d9d1b
Alex Naga <alex.naga@hotmail.com>
2020-03-11 10:30:41 +0100

    Move functions outside of render

--- lib/Cpu.jsx
+++ lib/Cpu.jsx
@@ -1,32 +1,32 @@
 import styles from './styles/styles.jsx';
 
+const cpuLimits = [
+  { limit: 3.3, icon: 'far fa-tachometer-fastest' },
+  { limit: 2.8, icon: 'far fa-tachometer-fast' },
+  { limit: 2.5, icon: 'far fa-tachometer' },
+  { limit: 2, icon: 'far fa-tachometer-average' },
+  { limit: 1.8, icon: 'far fa-tachometer-slow' },
+  { limit: 1.5, icon: 'far fa-tachometer-slowest' }
+];
+
+const getNearestValue = (array, value) => {
+  let result, lastDelta;
+
+  array.some(item => {
+    const delta = Math.abs(value - item.limit);
+    if (delta > lastDelta) return true;
+
+    result = item;
+    lastDelta = delta;
+  });
+  return result;
+};
+
 const render = ({ output }) => {
   if (!output) return;
 
   const cpuLoad = output.loadAverage;
-  const cpuLimits = [
-    { limit: 3.3, icon: 'far fa-tachometer-fastest' },
-    { limit: 2.8, icon: 'far fa-tachometer-fast' },
-    { limit: 2.5, icon: 'far fa-tachometer' },
-    { limit: 2, icon: 'far fa-tachometer-average' },
-    { limit: 1.8, icon: 'far fa-tachometer-slow' },
-    { limit: 1.5, icon: 'far fa-tachometer-slowest' }
-  ];
   const highCpuLoadLimit = cpuLimits[0].limit;
-
-  const getNearestValue = (array, value) => {
-    let result, lastDelta;
-
-    array.some(item => {
-      const delta = Math.abs(value - item.limit);
-      if (delta > lastDelta) return true;
-
-      result = item;
-      lastDelta = delta;
-    });
-    return result;
-  };
-
   const iconClassName = getNearestValue(cpuLimits, cpuLoad).icon;
 
   const highLoadWarning = cpuLoad > highCpuLoadLimit ? { color: styles.colors.red } : null;
