66fd6e61c2d2aba3f512218d848f709c8656879f
Alex Naga <alex.naga@hotmail.com>
2020-03-11 10:29:11 +0100

    Use dynamic CPU load icon

--- lib/Cpu.jsx
+++ lib/Cpu.jsx
@@ -3,16 +3,37 @@ import styles from './styles/styles.jsx';
 const render = ({ output }) => {
   if (!output) return;
 
-  const cpuLimit = 3;
-  const isHighLoad = output.loadAverage > cpuLimit;
+  const cpuLoad = output.loadAverage;
+  const cpuLimits = [
+    { limit: 3.3, icon: 'far fa-tachometer-fastest' },
+    { limit: 2.8, icon: 'far fa-tachometer-fast' },
+    { limit: 2.5, icon: 'far fa-tachometer' },
+    { limit: 2, icon: 'far fa-tachometer-average' },
+    { limit: 1.8, icon: 'far fa-tachometer-slow' },
+    { limit: 1.5, icon: 'far fa-tachometer-slowest' }
+  ];
+  const highCpuLoadLimit = cpuLimits[0].limit;
 
-  const hasWarning = isHighLoad ? { color: styles.colors.red } : null;
+  const getNearestValue = (array, value) => {
+    let result, lastDelta;
 
-  // TODO: There are even more cpu icons states on FA
+    array.some(item => {
+      const delta = Math.abs(value - item.limit);
+      if (delta > lastDelta) return true;
+
+      result = item;
+      lastDelta = delta;
+    });
+    return result;
+  };
+
+  const iconClassName = getNearestValue(cpuLimits, cpuLoad).icon;
+
+  const highLoadWarning = cpuLoad > highCpuLoadLimit ? { color: styles.colors.red } : null;
 
   return (
-    <div style={hasWarning}>
-      <i className="far fa-tachometer-fast" />
+    <div style={highLoadWarning}>
+      <i className={iconClassName} />
       {output.loadAverage}
     </div>
   );
